6a25ad0d61380ccf84c3825c191dabd935f04532
AxlMacias <axel.macias@ucb.edu.bo>
2024-09-26 03:08:54 -0400

    26/09/24_05 funcion while

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -8,24 +8,27 @@ cout<<"**********************************"<<endl;
 
 const int NUMERO_SECRETO=42;
 int adivina;
+bool no_acerto=true;
 
-cout<<"Cuál es el número? ";
-cin>>adivina;
-cout<<"El valor de su número es: "<<adivina<<endl;
+while(no_acerto){
+	cout<<"Cuál es el número? ";
+	cin>>adivina;
+	cout<<"El valor de su número es: "<<adivina<<endl;
 
-bool acerto=adivina==NUMERO_SECRETO;
-bool mayor=adivina>NUMERO_SECRETO;
+	bool acerto=adivina==NUMERO_SECRETO;
+	bool mayor=adivina>NUMERO_SECRETO;
 
-if(acerto){
-	cout<<"Felicitaciones!!! Adivinaste el número secreto!"<<endl;
-}
-
-else if(mayor){
-	cout<<"El número ingresado es mayor que el número secreto"<<endl;
+	if(acerto){
+		cout<<"Felicitaciones!!! Adivinaste el número secreto!"<<endl;
 	}
 
-else{
-	cout<<"El número ingresado es menor que el número secreto"<<endl;
+	else if(mayor){
+		cout<<"El número ingresado es mayor que el número secreto"<<endl;
+	}
+
+	else{
+		cout<<"El número ingresado es menor que el número secreto"<<endl;
+	}
 }
 
 }
