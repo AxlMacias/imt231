366487659a5d5bdaf7048e188a5d43a73ee7fdb2
AxlMacias <axel.macias@ucb.edu.bo>
2024-09-26 02:49:52 -0400

    26/09/24_03 funciones if/else

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -6,12 +6,19 @@ cout<<"**********************************";
 cout<<" Bienvenido al Juego de la Adivinanza!";
 cout<<"**********************************"<<endl;
 
-int numero_secreto=42;
-cout<<"El número secreto es "<<numero_secreto<<". No lo diga a nadie!"<<endl;
-
-int adivina;
+int numero_secreto=42, adivina;
 cout<<"Cuál es el número? ";
 cin>>adivina;
 cout<<"El valor de su número es: "<<adivina<<endl;
+
+if(adivina==numero_secreto){
+	cout<<"Felicitaciones!!! Adivinaste el número secreto!"<<endl;
+}
+else if(adivina>numero_secreto){
+	cout<<"El número ingresado es mayor que el número secreto"<<endl;
+	}
+else{
+	cout<<"El número ingresado es menor que el número secreto"<<endl;
+}
 
 }
