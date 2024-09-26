a533163a25b9b4fcc0b2e1f6cbf20319e874a365
AxlMacias <axel.macias@ucb.edu.bo>
2024-09-26 02:42:58 -0400

    26/09/24_02 variable adivina

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -9,4 +9,9 @@ cout<<"**********************************"<<endl;
 int numero_secreto=42;
 cout<<"El número secreto es "<<numero_secreto<<". No lo diga a nadie!"<<endl;
 
+int adivina;
+cout<<"Cuál es el número? ";
+cin>>adivina;
+cout<<"El valor de su número es: "<<adivina<<endl;
+
 }
