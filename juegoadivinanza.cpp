1e6e31eeab4dd32d9db3d63154bc1ca4eeff76d8
AxlMacias <axel.macias@ucb.edu.bo>
2024-09-26 02:28:37 -0400

    primer codigo

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -6,21 +6,4 @@ cout<<"**********************************";
 cout<<" Bienvenido al Juego de la Adivinanza!";
 cout<<"**********************************"<<endl;
 
-const int numero_secreto = 42;
-int adivina;
-bool acerto  =  adivina = numero_secreto;
-bool mayor = adivina > numero_secreto;
-
-cout<<"Dime un número: ";
-cin>>adivina;
-if(acerto){
-	cout<<"La paraste de pecho!!1!"<<endl;
-	}
-else if(mayor){
-	cout<<"Muy alto, sonso"<<endl;
-	}
-else{
-	cout<<"Muy bajo, choco"<<endl;
-	}
-
 }
